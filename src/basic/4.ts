7f516685cc0d773f62995f9bcd93f7fb37a66d35
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-07-10 20:33:26 +0100

    +

--- src/basic/4.ts
+++ src/basic/4.ts
@@ -1,11 +1,13 @@
-function calc(num1: number | string, num2: number | string) {
+function showMessage(message: string): void {
+  console.log(message);
+}
+function calc(num1: number | string, num2: number | string): number | string {
   if (typeof num1 === "number" && typeof num2 === "number") {
     return num1 + num2;
   } else {
     return num1.toString() + num2.toString();
   }
 }
-
 function customError(message: string): never {
   throw new Error(message);
 }
