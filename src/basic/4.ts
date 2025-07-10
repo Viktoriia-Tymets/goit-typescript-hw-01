64d90fbfb4e3d5b7242206f21e78e59e308c27c2
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-07-10 20:12:52 +0100

    +

--- src/basic/4.ts
+++ src/basic/4.ts
@@ -1,15 +1,15 @@
 function showMessage(message: string) {
-  console.log(message);
+  console.log(message);
 }
 
 function calc(num1: number | string, num2: number | string) {
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
-        return num1 + num2;
-    } else {
-        return num1.toString() + num2.toString()
-    }
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else {
+    return num1.toString() + num2.toString();
+  }
 }
 
 function customError(message: string): never {
-  throw new Error(message: 'Error');
+  throw new Error(message);
 }
