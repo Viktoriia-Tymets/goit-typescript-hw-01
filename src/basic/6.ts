354a08820f8f8e0cb04e25ac2b540928e4c873fa
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-07-10 20:06:13 +0100

    done

---
+++ src/basic/6.ts
@@ -0,0 +1,15 @@
+type User = {
+  name: string;
+  age: number;
+  email: string;
+};
+
+let user: User = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
