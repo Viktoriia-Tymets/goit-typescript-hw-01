454902bdfe26ba8373c2ea780f927db795eac0ea
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-07-10 21:16:17 +0100

    +

--- src/basic/5.ts
+++ src/basic/5.ts
@@ -1,14 +1,13 @@
 enum DayOfWeek {
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
-  Sunday
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
 }
-
 
 const isWeekend = (day: DayOfWeek): boolean => {
-    return day === DayOfWeek.Saturday || DayOfWeek.Sunday;
-}
+  return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
+};
